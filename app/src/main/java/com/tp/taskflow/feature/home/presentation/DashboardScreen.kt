19a6559747d7ad0a23dbf5ae9ce6932643380c85
PhoneThetKhine <phonethetkhine@codigo.co>
2026-09-15 17:49:41 +0630

    Fix : Dashboard spacing issue

--- app/src/main/java/com/tp/taskflow/feature/home/presentation/DashboardScreen.kt
+++ app/src/main/java/com/tp/taskflow/feature/home/presentation/DashboardScreen.kt
@@ -52,7 +52,7 @@ fun DashboardScreen(
                 horizontalArrangement = Arrangement.SpaceBetween,
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                Column {
+                Column(Modifier.weight(1f)) {
                     Text(
                         text = "TaskFlow",
                         style = MaterialTheme.typography.headlineMedium,
