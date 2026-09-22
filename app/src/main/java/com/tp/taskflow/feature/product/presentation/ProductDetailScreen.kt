d9223c91867c25b903571b5008dc7df62442a57a
PhoneThetKhine <phonethetkhine@codigo.co>
2026-09-22 18:00:30 +0630

    fix:remove import

--- app/src/main/java/com/tp/taskflow/feature/product/presentation/ProductDetailScreen.kt
+++ app/src/main/java/com/tp/taskflow/feature/product/presentation/ProductDetailScreen.kt
@@ -30,7 +30,6 @@ import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.semantics.contentDescription
-import androidx.compose.ui.semantics.mergeDescendants
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
